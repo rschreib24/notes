5252125fd7e8213a45c96e51a57fcd6b59a39b5a
rschreib24 <68935850+rschreib24@users.noreply.github.com>
2024-06-01 08:03:50 -0700

    Update bashrc-notes.sh

--- bashrc-notes.sh
+++ bashrc-notes.sh
@@ -1,5 +1,9 @@
 export PS1="\[\e[1;32m\]\u@\h\[\e[m\]:\[\e[1;34m\]\w\[\e[m\]\$ "
+# On Windows bash 
+# export PS1="\[\]\n\[\033[32m\] \[\033[35m\]$MSYSTEM \[\033[33m\]\w\[\033[36m\]`__git_ps1`\[\033[0m\]$ \[\]"
 export PROMPT_DIRTRIM=3
+
+alias al="vim ~/.bashrc"
 alias alc="code ~/.bashrc"
 alias ali="source ~/.bashrc"
 alias cdd="cd ~/code"
